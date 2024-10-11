3b9a3964de518ff5bde1ba0ab18f243ed469e748
Petr <p.koval@alta-group.ru>
2024-10-11 17:53:44 +0300

    add autocomplete search by project's title;

--- src/pages/projects/ui/projects.tsx
+++ src/pages/projects/ui/projects.tsx
@@ -1,8 +1,9 @@
-import {Table} from "antd";
 import {PageWrapper} from "@shared/ui/page-wrapper";
 import {Project} from "@entities/project";
 import {ProjectStatuses} from "@shared/enums";
 import {useCreateColumns} from "@pages/projects";
+import {AutoComplete, AutoCompleteProps, Button, Space, Table} from "antd";
+import React, {useCallback, useState} from "react";
 import {DateTime} from "luxon";
 
 import "../style/projects.scss";
@@ -51,18 +52,55 @@ const dataSource: Project[] = [
 
 export function Projects() {
 	const createColumns = useCreateColumns();
+	const [titleInput, setTitleInput] = useState('');
+	const [titleInputOptions, setTitleInputOptions] = useState<AutoCompleteProps['options']>([]);
+	const [filteredDataSource, setFilteredDataSource] = useState(dataSource);
 
 	const columns = createColumns();
 
+	const getTitlesFromData = useCallback((text: string) => {
+		if (text) {
+			const titles = dataSource.map(record => record.title);
+			const titlesIncludingText = titles.filter(title => title.toLowerCase().includes(text.toLowerCase()));
+
+			return titlesIncludingText.map(title => ({value: title}));
+		} else return [];
+	}, [dataSource]);
+
+	const filterDataSource = useCallback(() => {
+		const filteredDataSource = dataSource.filter(data => data.title.toLowerCase().includes(titleInput.toLowerCase()));
+
+		setFilteredDataSource(filteredDataSource);
+	}, [dataSource, titleInput]);
+
+	const handleInputEnterPress = (e: React.KeyboardEvent<HTMLDivElement>) => {
+		if (e.key === 'Enter') {
+			filterDataSource();
+		}
+	}
+
 	return (
 		<PageWrapper crumbs={[{title: 'Проекты'}]}>
+			<Space size="small">
+				<AutoComplete
+					style={{width: 200}}
+					value={titleInput}
+					onChange={text => setTitleInput(text)}
+					options={titleInputOptions}
+					onSearch={text => setTitleInputOptions(getTitlesFromData(text))}
+					placeholder="Поиск по названию"
+					onKeyDown={e => handleInputEnterPress(e)}
+				/>
 
+				<Button type="link" onClick={filterDataSource}>Поиск</Button>
+			</Space>
 
-			<Table<Project> dataSource={dataSource}
+			<Table<Project> dataSource={filteredDataSource}
 							columns={columns}
 							rowKey={record => record.id}
 							showSorterTooltip={false}
 							pagination={{position: ['bottomCenter']}}
+							style={{marginTop: "1rem"}}
 			/>
 		</PageWrapper>
 	)
